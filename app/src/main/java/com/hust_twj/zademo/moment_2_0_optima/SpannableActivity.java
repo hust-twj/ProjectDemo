da45b7db8bb152bf916590dd980bba0aff0764d8
tangwenjing <hust_twj@163.com>
2018-11-06 17:49:53 +0800

    图文混排

--- app/src/main/java/com/hust_twj/zademo/moment_2_0_optima/SpannableActivity.java
+++ app/src/main/java/com/hust_twj/zademo/moment_2_0_optima/SpannableActivity.java
@@ -3,13 +3,22 @@ package com.hust_twj.zademo.moment_2_0_optima;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.widget.TextView;
 
 import com.hust_twj.zademo.R;
 import com.hust_twj.zademo.moments_2_0.hot_topic.widget.ContentIntroductionLayout;
+import com.hust_twj.zademo.widget.CenterAlignedImageSpan;
 
 public class SpannableActivity extends Activity {
 
     private ContentIntroductionLayout contentIntroductionLayout;
+    private TextView mTv;
+    private SpannableString sSIcon;
+    private static final String SS_LAB = "#&&#";
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,5 +31,21 @@ public class SpannableActivity extends Activity {
                 + "你好啊你好啊你好啊你好啊5" +  "你好啊你好啊你好啊你好啊6"
                + "你好啊你好啊你好啊你好啊7" +  "你好啊你好啊你好啊你好啊8";
         contentIntroductionLayout.updateView(content);
+
+        mTv = findViewById(R.id.tv_span);
+        final SpannableStringBuilder spannableString;
+
+        spannableString = new SpannableStringBuilder("冒泡的茄子" + " " + "送" + " " + "生气的西米露" + " " + "冰淇淋");
+        final int length = spannableString.length();
+
+        sSIcon = new SpannableString(SS_LAB);
+        sSIcon.setSpan(new CenterAlignedImageSpan(this, R.drawable.heart_word_icon),
+                0, sSIcon.length(), Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+
+        mTv.setText(spannableString.toString());
+        mTv.append(" ");
+        mTv.append(sSIcon);
+        mTv.append(" x1 ");
+
     }
 }
