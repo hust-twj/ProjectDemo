ba5ef369dbb03afad82570fac1db49e6e35187d8
tangwenjing <hust_twj@163.com>
2019-03-25 10:01:05 +0800

    opti

--- app/src/main/java/com/hust_twj/zademo/ui_widget/VoiceProgressRing.java
+++ app/src/main/java/com/hust_twj/zademo/ui_widget/VoiceProgressRing.java
@@ -28,9 +28,12 @@ public class VoiceProgressRing extends View {
     private int progressEndColor;
     private int bgColor;
     private int dotColor;
-    private float progressWidth;//进度圆环的宽度
-    private float bgWidth;//背景宽度
-    private float dotWidth; //最后的小圆点的宽度
+    //进度圆环的宽度
+    private float progressWidth;
+    //背景宽度
+    private float bgWidth;
+    //最后的小圆点的半径
+    private float dotRadius;
 
     private Paint bgPaint;
     private Paint progressPaint;
@@ -40,9 +43,12 @@ public class VoiceProgressRing extends View {
 
     private static final int MAX_PROGRESS = 100;
     private static final int SWEEP_ANGLE = 360;
+    private static final int START_ANGLE = -90;
 
-    private int center;
+    private float center;
     private float radius;
+
+    private RectF mRectF;
 
     /**
      * 弧度
@@ -52,8 +58,17 @@ public class VoiceProgressRing extends View {
 
     private SweepGradient progressSeepGradient;
 
+    public VoiceProgressRing(Context context) {
+        this(context, null);
+    }
+
     public VoiceProgressRing(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs, 0);
+    }
+
+    public VoiceProgressRing(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
         TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.VoiceProgressRing);
         progressStartColor = ta.getColor(R.styleable.VoiceProgressRing_pr_progress_start_color, Color.YELLOW);
         progressMidColor = ta.getColor(R.styleable.VoiceProgressRing_pr_progress_mid_color, progressStartColor);
@@ -63,9 +78,13 @@ public class VoiceProgressRing extends View {
         mProgress = ta.getInt(R.styleable.VoiceProgressRing_pr_progress, 0);
         progressWidth = ta.getDimension(R.styleable.VoiceProgressRing_pr_progress_width, 2f);
         bgWidth = ta.getDimension(R.styleable.VoiceProgressRing_pr_bg_width, 1f);
-        dotWidth = ta.getDimension(R.styleable.VoiceProgressRing_pr_dot_radius, 2.5f);
+        dotRadius = ta.getDimension(R.styleable.VoiceProgressRing_pr_dot_radius, 2.5f);
         ta.recycle();
 
+        init();
+    }
+
+    private void init() {
         bgPaint = new Paint();
         bgPaint.setAntiAlias(true);
         bgPaint.setStyle(Paint.Style.STROKE);
@@ -82,14 +101,14 @@ public class VoiceProgressRing extends View {
         dotPaint = new Paint();
         dotPaint.setAntiAlias(true);
         dotPaint.setStyle(Paint.Style.FILL);
-        dotPaint.setStrokeWidth(dotWidth);
+        dotPaint.setStrokeWidth(dotRadius);
         dotPaint.setColor(dotColor);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        center = getWidth() / 2;
+        center = getWidth() / 2.0f;
         radius = (center - bgWidth / 2);// 半径
 
         radius = radius - bgWidth;
@@ -99,17 +118,16 @@ public class VoiceProgressRing extends View {
         drawProgress(canvas);
     }
 
-    private RectF mRectF;
-
     /**
      * 画进度圆环
      *
      * @param canvas canvas
      */
     private void drawProgress(Canvas canvas) {
-
+        //圆弧范围的外接矩形
         if (mRectF == null) {
-            mRectF = new RectF(center - radius, center - radius, center + radius, center + radius);//圆弧范围的外接矩形
+            mRectF = new RectF(center - radius, center - radius,
+                    center + radius, center + radius);
         }
 
         int progressColorSweep[] = {progressStartColor, progressMidColor, progressEndColor};
@@ -118,25 +136,25 @@ public class VoiceProgressRing extends View {
             progressSeepGradient = new SweepGradient(center, center, progressColorSweep, null);
         }
         //按照圆心旋转
-        matrix.setRotate(-90, center, center);
+        matrix.setRotate(START_ANGLE, center, center);
         progressSeepGradient.setLocalMatrix(matrix);
 
         for (int i = 0; i <= mAngle; i++) {
             progressPaint.setShader(progressSeepGradient);
         }
 
-        canvas.drawArc(mRectF, -90, mAngle, false, progressPaint);
+        canvas.drawArc(mRectF, START_ANGLE, mAngle, false, progressPaint);
 
-        if (mProgress > 0 && mProgress < 100) {
+        if (mProgress > 0 && mProgress < MAX_PROGRESS) {
             //平移画布之前保存之前画的
             canvas.save();
             // 将画布坐标原点移动至圆心
             canvas.translate(center, center);
             // 旋转和进度相同的角度,因为进度是从-90度开始的所以-90度
-            canvas.rotate(mAngle - 90);
+            canvas.rotate(mAngle + START_ANGLE);
             // 同理从圆心出发直接将原点平移至要画小球的位置
             canvas.translate(radius, 0);
-            canvas.drawCircle(0, 0, dotWidth, dotPaint);
+            canvas.drawCircle(0, 0, dotRadius, dotPaint);
             // 画完之后恢复画布坐标
             canvas.restore();
         }
