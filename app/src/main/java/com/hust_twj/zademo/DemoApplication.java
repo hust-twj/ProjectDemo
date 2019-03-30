3f28aadb1e6691b2104d0ff5d9172aff76cac833
tangwenjing <hust_twj@163.com>
2019-03-30 10:30:51 +0800

    LeakCanary

--- app/src/main/java/com/hust_twj/zademo/DemoApplication.java
+++ app/src/main/java/com/hust_twj/zademo/DemoApplication.java
@@ -4,23 +4,31 @@ import android.app.Application;
 import android.content.Context;
 
 import com.hust_twj.zademo.utils.CrashHandler;
+import com.squareup.leakcanary.LeakCanary;
 
 /**
  * Created by Wenjing.Tang
  * on 2018/12/20
  */
-public class App extends Application {
+public class DemoApplication extends Application {
 
-    public static App app;
+    public static DemoApplication app;
 
     @Override
     public void onCreate() {
         super.onCreate();
 
         CrashHandler.getInstance().init(this);
+
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            return;
+        }
+        LeakCanary.install(this);
+
+
     }
 
-    public static App getApp() {
+    public static DemoApplication getApp() {
         return app;
     }
 
