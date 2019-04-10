fb6792e64f3a0408d6c24fe0a7f2575c219e4d09
tangwenjing <hust_twj@163.com>
2019-04-10 10:06:21 +0800

    [feature]完善UI参数

--- app/src/main/java/com/hust_twj/zademo/ui_params/UIParamsActivity.java
+++ app/src/main/java/com/hust_twj/zademo/ui_params/UIParamsActivity.java
@@ -1,15 +1,17 @@
 package com.hust_twj.zademo.ui_params;
 
+import android.animation.Animator;
+import android.animation.ObjectAnimator;
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
 import android.widget.TextView;
 
 import com.hust_twj.zademo.R;
 import com.hust_twj.zademo.utils.LogUtils;
-import com.hust_twj.zademo.xfermode.ScratchCardActivity;
 
 public class UIParamsActivity extends Activity {
 
@@ -27,17 +29,74 @@ public class UIParamsActivity extends Activity {
             @Override
             public void run() {
                 LogUtils.e("twj124", mTvUIParams.getLeft() + "  " + mTvUIParams.getTop() +
-                        "  " + mTvUIParams.getRight() + "  " + mTvUIParams.getBottom());
-
-                LogUtils.e("twj124", mTvUIParams.getX());
+                        "  " + mTvUIParams.getRight() + "  " + mTvUIParams.getBottom() + "  " +
+                        mTvUIParams.getX() + "  " + mTvUIParams.getTranslationX());
             }
         });
 
     }
 
     public void scratchCard(View view){
-        startActivity(new Intent(this, ScratchCardActivity.class));
+       doObjectAnimation();
+       //doViewAnimation();
+    }
 
+    private void doObjectAnimation(){
+        ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(mTvUIParams, "translationX", mTvUIParams.getX(), 300);
+
+        objectAnimator.setDuration(3000);
+        objectAnimator.start();
+        objectAnimator.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                //60  60  360  360  360.0  300.0
+                LogUtils.e("twj124", "end  "+  mTvUIParams.getLeft() + "  " + mTvUIParams.getTop() +
+                        "  " + mTvUIParams.getRight() + "  " + mTvUIParams.getBottom() + "  " +
+                        mTvUIParams.getX() + "  " + mTvUIParams.getTranslationX());
+
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+    }
+
+    private void doViewAnimation() {
+        TranslateAnimation translateAnimation = new TranslateAnimation(Animation.RELATIVE_TO_SELF,0,Animation.RELATIVE_TO_SELF,0.5f,
+                Animation.RELATIVE_TO_SELF,0,Animation.RELATIVE_TO_SELF,0);
+        translateAnimation.setDuration(3000);
+        mTvUIParams.startAnimation(translateAnimation);
+        translateAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                // 60  60  360  360  60.0  0.0
+                LogUtils.e("twj124", "end  "+  mTvUIParams.getLeft() + "  " + mTvUIParams.getTop() +
+                        "  " + mTvUIParams.getRight() + "  " + mTvUIParams.getBottom() + "  " +
+                        mTvUIParams.getX() + "  " + mTvUIParams.getTranslationX());
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
     }
 
 }
