6bc1426e2103070f14476483c94da0ee8823415f
tangwenjing <hust_twj@163.com>
2019-04-27 17:54:24 +0800

    [feature]Application中增加生命周期方法的注册

--- app/src/main/java/com/hust_twj/zademo/DemoApplication.java
+++ app/src/main/java/com/hust_twj/zademo/DemoApplication.java
@@ -1,9 +1,12 @@
 package com.hust_twj.zademo;
 
+import android.app.Activity;
 import android.app.Application;
 import android.content.Context;
+import android.os.Bundle;
 
 import com.hust_twj.zademo.utils.CrashHandler;
+import com.hust_twj.zademo.utils.LogUtils;
 import com.squareup.leakcanary.LeakCanary;
 
 /**
@@ -24,6 +27,8 @@ public class DemoApplication extends Application {
             return;
         }
         LeakCanary.install(this);
+
+        registerLifeCallback();
     }
 
     public static DemoApplication getApp() {
@@ -32,6 +37,47 @@ public class DemoApplication extends Application {
 
     public static Context getContext() {
         return app.getApplicationContext();
+    }
+
+    private void registerLifeCallback() {
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+            @Override
+            public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+                LogUtils.e("tw234", "onActivityCreated");
+            }
+
+            @Override
+            public void onActivityStarted(Activity activity) {
+                LogUtils.e("tw234", "onActivityStarted");
+            }
+
+            @Override
+            public void onActivityResumed(Activity activity) {
+                LogUtils.e("tw234", "onActivityResumed");
+            }
+
+            @Override
+            public void onActivityPaused(Activity activity) {
+                LogUtils.e("tw234", "onActivityPaused");
+            }
+
+            @Override
+            public void onActivityStopped(Activity activity) {
+                LogUtils.e("tw234", "onActivityStopped");
+            }
+
+            @Override
+            public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+                LogUtils.e("tw234", "onActivitySaveInstanceState");
+            }
+
+            @Override
+            public void onActivityDestroyed(Activity activity) {
+                LogUtils.e("tw234", "onActivityDestroyed");
+
+            }
+        });
+
     }
 
 }
