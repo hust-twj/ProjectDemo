1488ac10831745a06c3b831f625f4b008048cb6a
hust_twj <254062403@qq.com>
2019-06-18 00:27:32 +0800

    [feature]水平数据转换为垂直数据

--- app/src/main/java/com/hust_twj/zademo/ui_widget/activity/GridRvActivity.java
+++ app/src/main/java/com/hust_twj/zademo/ui_widget/activity/GridRvActivity.java
@@ -18,6 +18,9 @@ import com.hust_twj.zademo.ui_widget.widget.CircleRecyclerView;
 import com.hust_twj.zademo.utils.DensityUtils;
 import com.hust_twj.zademo.utils.LogUtils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * description ：双层列表
  * Created by Wenjing.Tang on 2019-05-24.
@@ -29,9 +32,11 @@ public class GridRvActivity extends Activity {
     private LinearLayoutManager mLayoutManager;
     private static final int SPAN_COUNT = 2;
 
-    private int[] mImgs = {R.drawable.img_1, R.drawable.img_2, R.drawable.img_3, R.drawable.img_4,
-            R.drawable.img_5, R.drawable.img_6, R.drawable.img_1, R.drawable.img_2, R.drawable.img_3, R.drawable.img_4,
+    private int[] mImgs = {R.drawable.img_1, R.drawable.img_2, R.drawable.img_3, R.drawable.img_4, R.drawable.img_5,
+            R.drawable.img_6, R.drawable.img_1, R.drawable.img_2, R.drawable.img_3, R.drawable.img_4,
             R.drawable.img_5, R.drawable.img_6, R.drawable.img_1, R.drawable.img_2};
+
+    private String[] mIndex = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"};
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -49,6 +54,58 @@ public class GridRvActivity extends Activity {
         mCircleRecyclerView.setDrawBg(true);
         mCircleRecyclerView.setAdapter(new A());
 
+        List<String> list = new ArrayList<>();
+
+        for (int i = 0; i < mImgs.length -1; i++) {
+            list.add(String.valueOf(i));
+        }
+
+        LogUtils.a("twj", list.toString());
+        convertData(list);
+    }
+
+    /**
+     * 将横向数据转换为纵向数据
+     */
+    private List<String> convertData(List<String> original) {
+        //结果
+        List<String> result = new ArrayList<>();
+        //第1行
+        List<String> firstRow = new ArrayList<>();
+        //第2行
+        List<String> secondRow = new ArrayList<>();
+
+        if (original.size() == 0) {
+            return result;
+        }
+        //列数
+        int span = (original.size() + 1) / 2;
+
+        for (int i = 0; i < span; i++) {
+            firstRow.add(original.get(i));
+        }
+
+        for (int i = span; i < original.size(); i++) {
+            secondRow.add(original.get(i));
+        }
+
+        LogUtils.a("twj", firstRow.toString() +" ||  " +  secondRow.toString());
+        // [0, 1, 2, 3, 4, 5, 6]
+
+        for (int i = 0; i < firstRow.size(); i++) {
+            if (i % 2 == 1) {
+                firstRow.add(i, secondRow.get(0));
+                secondRow.remove(0);
+            }
+        }
+
+        if (secondRow.size() != 0) {
+            firstRow.addAll(secondRow);
+        }
+
+        LogUtils.a("twj", firstRow.toString() );
+
+        return result;
     }
 
     class A extends RecyclerView.Adapter<VH> {
@@ -88,6 +145,11 @@ public class GridRvActivity extends Activity {
             tv = itemView.findViewById(R.id.item_text);
             iv = itemView.findViewById(R.id.item_img);
         }
+    }
+
+    public static class Menu {
+        String image;
+        String title;
     }
 
     private class ItemDecoration extends RecyclerView.ItemDecoration {
