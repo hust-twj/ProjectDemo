e12806bd74951c0bdf174c9c6b93a6769f0cd196
tangwenjing <hust_twj@163.com>
2019-07-09 15:37:22 +0800

    [feature]添加RxJava

--- app/src/main/java/com/hust_twj/zademo/third_part/rx_java/RxJavaActivity.java
+++ app/src/main/java/com/hust_twj/zademo/third_part/rx_java/RxJavaActivity.java
@@ -3,8 +3,14 @@ package com.hust_twj.zademo.third_part.rx_java;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.hust_twj.zademo.R;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
 
 /**
  * RxJava使用：
@@ -17,6 +23,38 @@ public class RxJavaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_rx_java);
+
+        Observable.just("Hello", "world")
+                .subscribe(new Consumer<String>() {
+                    @Override public void accept(String s) {
+                        Log.e("twj124", "accept: " +s);
+                    }
+                });
+
+        Observable.just("haha1","haha2","haha3")
+                .subscribe(new Observer<String>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        Log.e("twj124", "onSubscribe");
+                    }
+
+                    @Override
+                    public void onNext(String s) {
+                        Log.e("twj124", "onNext: " +s);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.e("twj124", "onError: ");
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.e("twj124", "onComplete: ");
+                    }
+                })
+                ;
+
     }
 
 }
