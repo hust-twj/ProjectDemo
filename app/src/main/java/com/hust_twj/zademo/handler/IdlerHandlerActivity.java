52035c088b7f1294724ba1f86855b273e990a11c
hust_twj <254062403@qq.com>
2019-07-28 00:28:27 +0800

    使用HandlerThread创建子线程并quit进行线程终止；removeIdleHandler移除

--- app/src/main/java/com/hust_twj/zademo/handler/IdlerHandlerActivity.java
+++ app/src/main/java/com/hust_twj/zademo/handler/IdlerHandlerActivity.java
@@ -2,14 +2,16 @@ package com.hust_twj.zademo.handler;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.os.Looper;
+import android.os.Message;
 import android.os.MessageQueue;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.View;
 
 import com.hust_twj.zademo.R;
-
 
 /**
  * IdlerHandler
@@ -18,19 +20,43 @@ import com.hust_twj.zademo.R;
  */
 public class IdlerHandlerActivity extends Activity {
 
+    private HandlerThread handlerThread ;
+    private Handler mHandler;
+
+    private MyIdleHandler mIdleHandler;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_idle_handler);
 
-        Log.e("twj125", "onCreate：" + System.currentTimeMillis());
-        Looper.myQueue().addIdleHandler(new MyIdleHandler());
+        handlerThread = new HandlerThread("handlerThread");
+        handlerThread.start();
+        Log.e("twj125", "onCreate：" + System.currentTimeMillis() +  "  " + Thread.currentThread().getName());
+        mIdleHandler = new MyIdleHandler();
+        Looper.myQueue().addIdleHandler(mIdleHandler);
+
+        mHandler = new Handler(handlerThread.getLooper()) {
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                //子线程中执行 模拟耗时操作
+                Log.e("twj125", "handleMessage start：" + System.currentTimeMillis() +  "  " + Thread.currentThread().getName());
+                try {
+                    Thread.sleep(4000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Log.e("twj125", "handleMessage   end：" + System.currentTimeMillis() +  "  " + Thread.currentThread().getName());
+            }
+        };
     }
 
     public void click(View view) {
+        //主线程中执行
         Log.e("twj125", "click start：" + System.currentTimeMillis());
         try {
-            Thread.sleep(4000);
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -41,23 +67,23 @@ public class IdlerHandlerActivity extends Activity {
 
         @Override
         public boolean queueIdle() {
-            new MyThread().start();
+            Log.e("twj125", "queueIdle：" + System.currentTimeMillis() +  "  " + Thread.currentThread().getName());
+            if (mHandler != null) {
+                Message message = Message.obtain();
+                mHandler.sendMessage(message);
+            }
             return true;
         }
     }
 
-    class MyThread extends Thread {
-        @Override
-        public void run() {
-            Log.e("twj125", "queueIdle start：" + System.currentTimeMillis());
-            try {
-                Thread.sleep(4000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            Log.e("twj125", "queueIdle   end：" + System.currentTimeMillis());
-            super.run();
-        }
+    @Override
+    protected void onDestroy() {
+        handlerThread.quitSafely();
+        mHandler.removeCallbacksAndMessages(null);
+        mHandler = null;
+        //移除IdleHandle， 防止queueIdle()一直执行
+        Looper.myQueue().removeIdleHandler(mIdleHandler);
+        super.onDestroy();
     }
 
 }
