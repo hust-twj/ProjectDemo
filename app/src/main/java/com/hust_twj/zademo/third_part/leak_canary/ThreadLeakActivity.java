3dcad05e1929d2e6b748f6f3e6168f3444b4ca88
hust_twj <254062403@qq.com>
2019-08-31 23:33:03 +0800

    完善线程泄露

--- app/src/main/java/com/hust_twj/zademo/third_part/leak_canary/ThreadLeakActivity.java
+++ app/src/main/java/com/hust_twj/zademo/third_part/leak_canary/ThreadLeakActivity.java
@@ -3,11 +3,11 @@ package com.hust_twj.zademo.third_part.leak_canary;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 import com.hust_twj.zademo.R;
 
 public class ThreadLeakActivity extends Activity {
-
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -15,7 +15,20 @@ public class ThreadLeakActivity extends Activity {
 
         setContentView(R.layout.activity_leak_canary);
 
+        new MyThread().start();
 
+    }
+
+    public class MyThread extends Thread  {
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(5000);
+                Log.d("twj", "执行了多线程");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
