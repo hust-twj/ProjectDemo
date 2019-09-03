756f24fac4a8cc5801710cdaa40bb01d5542c374
tangwenjing <hust_twj@163.com>
2019-09-03 20:23:07 +0800

    [feature]打印出堆大小

--- app/src/main/java/com/hust_twj/zademo/jvm/JVMActivity.kt
+++ app/src/main/java/com/hust_twj/zademo/jvm/JVMActivity.kt
@@ -1,11 +1,14 @@
 package com.hust_twj.zademo.jvm
 
 import android.app.Activity
+import android.app.ActivityManager
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 
 import com.hust_twj.zademo.R
 import com.hust_twj.zademo.jvm.invoke.InvokeActivity
+import com.hust_twj.zademo.utils.LogUtils
 import kotlinx.android.synthetic.main.activity_jvm.*
 
 /**
@@ -13,6 +16,8 @@ import kotlinx.android.synthetic.main.activity_jvm.*
  * on 2019/3/30
  */
 class JVMActivity : Activity() {
+
+    private lateinit var manager: ActivityManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +31,11 @@ class JVMActivity : Activity() {
         tv_invoke.setOnClickListener {
             startActivity(Intent(this@JVMActivity, InvokeActivity::class.java))
         }
+
+        manager = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        val heapSize = manager.memoryClass
+        //384
+        LogUtils.e("twj", heapSize)
 
     }
 
