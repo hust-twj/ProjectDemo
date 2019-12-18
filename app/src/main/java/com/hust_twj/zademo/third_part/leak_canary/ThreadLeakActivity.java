651ccc3669f40da55f42c4e9bdd982d1c4b74a45
tangwenjing <hust_twj@163.com>
2019-12-18 15:34:15 +0800

    [feature]打印角度

--- app/src/main/java/com/hust_twj/zademo/third_part/leak_canary/ThreadLeakActivity.java
+++ app/src/main/java/com/hust_twj/zademo/third_part/leak_canary/ThreadLeakActivity.java
@@ -7,8 +7,8 @@ import android.util.Log;
 
 import com.hust_twj.zademo.R;
 
-/**
- * 线程引起的内存泄露
+/**线程引起的内存泄露
+ *
  * Created by Wenjing.Tang
  * on 2019/8/5
  */
@@ -20,9 +20,23 @@ public class ThreadLeakActivity extends Activity {
         setContentView(R.layout.activity_thread_leak);
 
         new MyThread().start();
+
+        new MyThread2().start();
     }
 
     public class MyThread extends Thread  {
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(10 * 1000);
+                Log.i("twj", "执行了子线程");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public class MyThread2 extends Thread  {
         @Override
         public void run() {
             try {
--- app/src/main/java/com/hust_twj/zademo/ui_widget/activity/GradientDrawableActivity.java
+++ app/src/main/java/com/hust_twj/zademo/ui_widget/activity/GradientDrawableActivity.java
@@ -1,10 +1,13 @@
 package com.hust_twj.zademo.ui_widget.activity;
 
 import android.app.Activity;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.TextView;
 
 import com.hust_twj.zademo.R;
+import com.hust_twj.zademo.utils.LogUtils;
 
 /**
  * Created by hust_twj
@@ -18,6 +21,18 @@ public class GradientDrawableActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_gradient_drawable);
+
+        TextView  textView = findViewById(R.id.tv_bg);
+        LogUtils.e("twj12", textView.getBackground());
+
+        GradientDrawable drawable = (GradientDrawable) textView.getBackground();
+        drawable.getConstantState();
+        //drawable.getOrientation();
+
+        LogUtils.e("twj12", drawable.getOrientation());
+
+
+        // textView.setText("haha");
 
     }
 
