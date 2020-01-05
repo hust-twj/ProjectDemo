eff13d0134374fa954c0728dc1d78232e2b847bf
hust_twj <254062403@qq.com>
2020-01-06 01:32:22 +0800

    [fix]修复编译问题

--- app/src/main/java/com/hust_twj/zademo/third_part/retrofit/RetrofitActivity.java
+++ app/src/main/java/com/hust_twj/zademo/third_part/retrofit/RetrofitActivity.java
@@ -12,13 +12,13 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * Description ：
  * Created by Wenjing.Tang on 2020-01-04.
  */
 public class RetrofitActivity extends Activity {
-
 
     private TextView mTvResult;
 
@@ -30,33 +30,35 @@ public class RetrofitActivity extends Activity {
 
         mTvResult = findViewById(R.id.tv_result);
 
+        request();
+    }
+
+    private void request() {
         // 在创建Retrofit实例时通过.baseUrl()设置
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://fanyi.youdao.com/") //设置网络请求的Url地址
-                // .addConverterFactory(GsonConverterFactory.create()) //设置数据解析器
+                .baseUrl("https://api.github.com/") // 设置 网络请求 Url
+                .addConverterFactory(GsonConverterFactory.create()) //设置数据解析器
                 .build();
 
         // 创建 网络请求接口 的实例
         ServiceInterface service = retrofit.create(ServiceInterface.class);
 
         //对 发送请求 进行封装
-        Call<Translation> call = service.getCall();
+        Call<GitUsers> call = service.getCall();
 
-        call.enqueue(new Callback<Translation>() {
+        call.enqueue(new Callback<GitUsers>() {
             @Override
-            public void onResponse(Call<Translation> call, Response<Translation> response) {
+            public void onResponse(Call<GitUsers> call, Response<GitUsers> response) {
                 String result = response.toString();
                 LogUtils.e("twj124", result);
-
             }
 
             @Override
-            public void onFailure(Call<Translation> call, Throwable t) {
+            public void onFailure(Call<GitUsers> call, Throwable t) {
                 LogUtils.e("twj124", "onFailure");
 
             }
         });
-
     }
 
 }
--- app/src/main/java/com/hust_twj/zademo/third_part/retrofit/ServiceInterface.java
+++ app/src/main/java/com/hust_twj/zademo/third_part/retrofit/ServiceInterface.java
@@ -1,6 +1,10 @@
 package com.hust_twj.zademo.third_part.retrofit;
 
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 /**
  * Description ：
@@ -8,6 +12,11 @@ import retrofit2.Call;
  */
 public interface ServiceInterface {
 
-    @GET("openapi.do?keyfrom=Yanzhikai&key=2032414398&type=data&doctype=json&version=1.1&q=car")
-    Call<Translation> getCall();
+    //https://api.github.com/users/hust-twj/repos
+    //@GET("openapi.do?keyfrom=Yanzhikai&key=2032414398&type=data&doctype=json&version=1.1&q=car")
+    @GET("users")
+   // @POST("translate?doctype=json&jsonversion=&type=&keyfrom=&model=&mid=&imei=&vendor=&screen=&ssid=&network=&abtest=")
+   // @FormUrlEncoded
+    //Call<Translation> getCall(@Path("user") String user);
+    Call<GitUsers> getCall();
 }
