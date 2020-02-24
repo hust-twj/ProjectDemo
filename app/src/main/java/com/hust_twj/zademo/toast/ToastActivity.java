d72b796844f12dde3a06783f86510b243b14cc46
tangwenjing <hust_twj@163.com>
2020-02-24 16:05:13 +0800

    [feature]测试：子线程中有弹出Toast，然后Toast并没有关闭，又在主线程弹出了同一个对象的toast，会造成崩溃

--- app/src/main/java/com/hust_twj/zademo/toast/ToastActivity.java
+++ app/src/main/java/com/hust_twj/zademo/toast/ToastActivity.java
@@ -3,8 +3,11 @@ package com.hust_twj.zademo.toast;
 import android.app.Activity;
 import android.os.Bundle;
 import androidx.annotation.Nullable;
+
+import android.os.Looper;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.hust_twj.zademo.R;
 import com.hust_twj.zademo.toast.utils.ToastUtils;
@@ -13,6 +16,7 @@ public class ToastActivity extends Activity {
 
     private TextView mtvToast;
 
+    private Toast sToast;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -32,4 +36,32 @@ public class ToastActivity extends Activity {
         });
 
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        //子线程中有弹出过Toast，然后Toast并没有关闭，又在主线程弹出了同一个对象的toast，会造成崩溃。
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Looper.prepare();
+                sToast = Toast.makeText(ToastActivity.this,"子线程弹出Toast",Toast.LENGTH_SHORT);
+                sToast.show();
+                Looper.loop();
+            }
+        });
+        thread.start();
+    }
+
+    /**
+     * 子线程中有弹出过Toast，然后Toast并没有关闭，又在主线程弹出了同一个对象的toast，会造成崩溃。
+     * 解决：不在子线程中弹出toast--子线程弹toast时，统一利用handler转移到主线程中处理
+     * @param view
+     */
+    public void showToast(View view) {
+        sToast.setText("主线程弹出Toast");
+        sToast.show();
+    }
+
 }
