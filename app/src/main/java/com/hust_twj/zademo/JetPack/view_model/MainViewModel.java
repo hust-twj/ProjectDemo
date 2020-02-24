0ac5b252758a26fdfbf40f0801503d6a8f329dd9
tangwenjing <hust_twj@163.com>
2020-02-24 17:29:52 +0800

    [feature]添加ViewModel

---
+++ app/src/main/java/com/hust_twj/zademo/JetPack/view_model/MainViewModel.java
@@ -0,0 +1,13 @@
+package com.hust_twj.zademo.JetPack.view_model;
+
+import androidx.lifecycle.ViewModel;
+
+/**
+ * Description ：
+ * Created by Wenjing.Tang on 2020-02-18.
+ */
+public class MainViewModel extends ViewModel {
+
+    String content = "hello world";
+
+}
