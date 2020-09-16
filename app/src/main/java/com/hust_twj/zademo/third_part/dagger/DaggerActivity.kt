4fd7c6261ea8b329c040c8d72b19cb0c8716eb7d
husttwj <tangwenjing0514@gmail.com>
2020-09-17 00:55:25 +0800

    [feature]添加dagger依赖

---
+++ app/src/main/java/com/hust_twj/zademo/third_part/dagger/DaggerActivity.kt
@@ -0,0 +1,17 @@
+package com.hust_twj.zademo.third_part.dagger
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.hust_twj.zademo.R
+
+/**
+ * Dagger使用：
+ * Created by hust_twj on 2020-09-09.
+ */
+class DaggerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dagger)
+
+    }
+}
---
+++ app/src/main/java/com/hust_twj/zademo/third_part/dagger/Person.java
@@ -0,0 +1,9 @@
+package com.hust_twj.zademo.third_part.dagger;
+
+/**
+ * Description ：
+ * Created by Wenjing.Tang on 2020/9/17.
+ */
+class Person {
+
+}
